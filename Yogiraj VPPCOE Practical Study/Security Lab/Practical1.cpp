039538fa288b6117db5aeb7e0733e56c1bad2dc9
yogirajbshinde21 <yogirajbshinde21@gmail.com>
2024-07-15 23:09:10 +0530

    Adding security lab practicals

---
+++ Yogiraj VPPCOE Practical Study/Security Lab/Practical1.cpp
@@ -0,0 +1,67 @@
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+void encrypt(string s, int k, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        char ch = ((s[i] - 'a' + k) % 26) + 'a';
+        s[i] = ch;
+    }
+    cout << s;
+}
+
+void decrypt(string s, int k, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        // 'k' = 107-97 = 10-3 = 7%26 = 7+97 = 104 i.e. 'h'
+        char ch = ((s[i] - 'a' - k) % 26) + 'a';
+        s[i] = ch;
+    }
+    cout << s;
+}
+
+int main()
+{
+    string str;
+    cout << "Input string: ";
+    getline(cin, str); // Read the entire line including spaces
+    int key = 3;       // By default setting to key as 3.
+
+    int len = str.size();
+
+    // Remove spaces
+    string result;
+    for (char ch : str)
+    {
+        if (ch != ' ')
+        {
+            result += ch;
+        }
+    }
+
+    cout << "Press 1 --> for Encrypt" << endl;
+    cout << "Press 2 --> for Decrypt" << endl;
+
+    int d;
+    cin >> d;
+    switch (d)
+    {
+    case 1:
+        encrypt(result, key, len);
+        break;
+
+    case 2:
+        decrypt(result, key, len);
+        break;
+
+    default:
+        cout << "Your input is invalid!";
+        break;
+    }
+
+    return 0;
+}
